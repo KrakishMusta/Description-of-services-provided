4150077bac44febb8e51adff3d5fc1df7aba1e40
KrakishMusta <project.zero.one01@gmail.com>
2025-10-26 16:33:47 +0700

    фикс

--- DoSP/vite.config.js
+++ DoSP/vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/Description-of-services-provided/assets/",
+    base: "/Description-of-services-provided/",
     plugins: [vue(), vueDevTools(), tailwindcss()],
     resolve: {
         alias: {
