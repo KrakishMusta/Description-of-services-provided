b70391b5d988c1eb4914e63ba40b802edb03158f
KrakishMusta <project.zero.one01@gmail.com>
2025-10-26 16:27:26 +0700

    изменил расположение dist при сборке

--- DoSP/vite.config.js
+++ DoSP/vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/dosp/",
+    base: "/Description-of-services-provided/assets/",
     plugins: [vue(), vueDevTools(), tailwindcss()],
     resolve: {
         alias: {
