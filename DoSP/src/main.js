87b3486042ec80e7902b7d9abbaf419527821f83
KrakishMusta <project.zero.one01@gmail.com>
2025-11-04 15:33:27 +0700

    импорт стилей в main.js

--- DoSP/src/main.js
+++ DoSP/src/main.js
@@ -1,6 +1,7 @@
 import { ViteSSG } from "vite-ssg/single-page";
 import { createHead } from "@unhead/vue/client";
 import App from "./App.vue";
+import "./styles/main.css";
 
 export const createApp = ViteSSG(App, ({ app }) => {
     const head = createHead();
